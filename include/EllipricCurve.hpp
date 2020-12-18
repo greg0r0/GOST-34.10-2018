68582a0129aff92ad2d1bfbe0948199e851becf9
greg0r0 <game_grego@mail.ru>
2020-12-19 02:43:42 +0300

    Added curve secp256r1

--- include/EllipricCurve.hpp
+++ include/EllipricCurve.hpp
@@ -23,12 +23,13 @@ namespace ECC {
         T b;
         T G_x;
         T n;
+        T h;
 
         public:
 
-        EllipticCurve(T _a, T _b, T _modulo) : a(_a), b(_b), modulo(_modulo), G_x(nullptr), n(nullptr){}
+        EllipticCurve(T _a, T _b, T _modulo) : a(_a), b(_b), modulo(_modulo), G_x(nullptr), n(nullptr), h(nullptr){}
 
-        EllipticCurve(T _a, T _b, T _modulo, T _G_x, T _order) : a(_a), b(_b), modulo(_modulo), G_x(_G_x), n(_order){}
+        EllipticCurve(T _a, T _b, T _modulo, T _G_x, T _order, T _h) : a(_a), b(_b), modulo(_modulo), G_x(_G_x), n(_order), h(_h){}
 
         void setG_x(T x_coord){
             if (CheckPoint()){
@@ -56,11 +57,20 @@ namespace ECC {
 
     namespace StandartCurves
     {
+        //Bitcoin Curve, J(E)=0
         EllipticCurve<uint256_t> secp256k1(0,
                                            7,
                                            0xfffffffffffffffffffffffffffffffffffffffffffffffffffffffefffffc2f_cppui,
                                            0x79be667ef9dcbbac55a06295ce870b07029bfcdb2dce28d959f2815b16f81798_cppui,
-                                           0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141_cppui);
+                                           0xfffffffffffffffffffffffffffffffebaaedce6af48a03bbfd25e8cd0364141_cppui,
+                                           1);
+        //J(E)!=0
+        EllipticCurve<uint256_t> secp256r1(0xffffffff00000001000000000000000000000000fffffffffffffffffffffffc_cppui,
+                                           0x5ac635d8aa3a93e7b3ebbd55769886bc651d06b0cc53b0f63bce3c3e27d2604b_cppui,
+                                           0xffffffff00000001000000000000000000000000ffffffffffffffffffffffff_cppui,
+                                           0x6b17d1f2e12c4247f8bce6e563a440f277037d812deb33a0f4a13945d898c296_cppui,
+                                           0xffffffff00000000ffffffffffffffffbce6faada7179e84f3b9cac2fc632551_cppui,
+                                           1);
     }
 
 }
